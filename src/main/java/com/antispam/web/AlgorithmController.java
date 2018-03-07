17eaeb7605dca67e1b1e9fd01d6ccf6f0c4a5898
JOANAPASCOAL\Joana <joanacpascoal@gmail.com>
2018-03-07 21:58:02 +0000

    Started Spring Web Project

---
+++ src/main/java/com/antispam/web/AlgorithmController.java
@@ -0,0 +1,22 @@
+package com.antispam.web;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AlgorithmController {
+
+    // inject via application.properties
+    @Value("${welcome.message:test}")
+    private String message = "Hello World";
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "index";
+    }
+
+}
