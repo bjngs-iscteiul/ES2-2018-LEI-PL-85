597af96ba80fe2c4dfcea98645d7ca793750978f
Pedro Pascoal <pjmpl@iscte-iul.pt>
2018-03-18 21:05:32 +0000

    Integration With AntiSpamFilterProblem
    Integration with Old project
    Anti Spam Filter
    
    Class Add:
    -AutomaticConfiguration
    -Problem

---
+++ src/main/java/com/antispam/web/AntiSpamFilterAutomaticConfiguration.java
@@ -0,0 +1,80 @@
+package com.antispam.web;
+
+public class AutomaticConfiguration {
+    private final int INDEPENDENT_RUNS = 5 ;
+    private Problem problem ;
+
+
+    public AutomaticConfiguration(Problem problem){
+        this.problem = problem;
+    }
+
+
+    public void INIT(String AlgorithmName) throws IOException {
+        String experimentBaseDirectory = "experimentBaseDirectory";
+
+        List<ExperimentProblem<DoubleSolution>> problemList = new ArrayList<>();
+
+        problemList.add(new ExperimentProblem<>(Problem, "Problem"));
+
+        List<ExperimentAlgorithm<DoubleSolution, List<DoubleSolution>>> algorithmList =
+                configureAlgorithmList(problemList);
+
+
+        try{
+            Experiment<DoubleSolution, List<DoubleSolution>> experiment =
+                    new ExperimentBuilder<DoubleSolution, List<DoubleSolution>>("AntiSpamStudy")
+                            .setAlgorithmList(algorithmList)
+                            .setProblemList(problemList)
+                            .setExperimentBaseDirectory(experimentBaseDirectory)
+                            .setOutputParetoFrontFileName("FUN")
+                            .setOutputParetoSetFileName("VAR")
+                            .setReferenceFrontDirectory(experimentBaseDirectory+"/referenceFronts")
+                            .setIndicatorList(Arrays.asList(
+                                    new PISAHypervolume<DoubleSolution>()
+                            ))
+                            .setIndependentRuns(INDEPENDENT_RUNS)
+                            .setNumberOfCores(8)
+                            .build();
+
+
+            //Debug.getInstance().msg("experiment"+experiment.toString());
+
+            new ExecuteAlgorithms<>(experiment).run();
+            new GenerateReferenceParetoSetAndFrontFromDoubleSolutions(experiment).run();
+            new ComputeQualityIndicators<>(experiment).run() ;
+            new GenerateLatexTablesWithStatistics(experiment).run() ;
+            new GenerateBoxplotsWithR<>(experiment).setRows(1).setColumns(1).run() ;
+
+
+        } catch (Exception e) {
+            System.out.println("Erro no experiment jmetal");
+            //System.out.println("Experiment" + experiment);
+        }
+        Debug.OUT("AntiSpamFilterAutomaticConfiguration [MAIN]");
+    }
+
+    static List<ExperimentAlgorithm<DoubleSolution, List<DoubleSolution>>> configureAlgorithmList(
+            List<ExperimentProblem<DoubleSolution>> problemList) {
+
+        List<ExperimentAlgorithm<DoubleSolution, List<DoubleSolution>>> algorithms = new ArrayList<>();
+
+        for (int i = 0; i < problemList.size(); i++) {
+            Algorithm<List<DoubleSolution>> algorithm =
+                    new NSGAIIBuilder<>(
+                            problemList.get(i).getProblem(),
+                            new SBXCrossover(1.0, 5),
+                            new PolynomialMutation(1.0 / problemList.get(i).getProblem().getNumberOfVariables(), 10.0)
+                    ).setMaxEvaluations(25000)
+                            .setPopulationSize(100)
+                            .build();
+            algorithms.add(new ExperimentAlgorithm<>(algorithm, AlgorithmName, problemList.get(i).getTag()));
+        }
+        return algorithms;
+    }
+
+    public Problem getProblem() {
+        return this.problem;
+    }
+
+}
---
+++ src/main/java/com/antispam/web/Problem.java
@@ -0,0 +1,143 @@
+package com.antispam.web;
+
+public class Problem extends AbstractDoubleProblem {
+
+    private LinkedHashMap<String, ArrayList<String>> ham;
+    private LinkedHashMap<String, ArrayList<String>> spam;
+    private double countFP = 0.0;
+    private double countFN = 0.0;
+    private final double algorithmLimit = 5.0;
+    private static final long serialVersionUID = -8036102488474183100L;
+    private String pathHam;
+    private String pathSpam;
+    private String pathrules;
+
+    private LinkedHashMap <String,Double> rules = FileLoader.getInstance().getRulesMap();
+
+    public double getCountFP() {
+        return countFP;
+    }
+
+
+    public double getCountFN() {
+        return countFN;
+    }
+
+
+
+    public Problem() {
+        // 10 variables (anti-spam filter rules) by default
+        //TODO getNumberOfRules file rules
+        //FileLoader.getInstance().manualStart(pathrules, pathHam, pathSpam);
+        this(FileLoader.getInstance().getRulesMap().size());
+
+        //TODO Create GetPath's to Rules, Ham, Spam of GUI
+        Debug.getInstance();
+        Debug.IN("AntiSpamFilterProblem [Constructor]");
+        Debug.OUT("AntiSpamFilterProblem [Constructor]");
+    }
+
+
+    //definimos o lower limit e upper limit
+    /**
+     * Constructor.
+     * Creates a new instance of the AntiSpamFilter Problem.
+     *
+     * @param numberOfVariables Number of variables of the problem
+     */
+    public AntiSpamFilterProblem(Integer numberOfVariables) {
+        Debug.IN("AntiSpamFilterProblem [Constructor(INTEGER)]");
+
+        //TODO
+        ham = FileLoader.getInstance().getHamRulesMap();
+        spam= FileLoader.getInstance().getSpamRulesMap();
+
+
+        setNumberOfVariables(numberOfVariables);
+        setNumberOfObjectives(2);// FN & FP
+        setName("AntiSpamFilterProblem");
+
+        List<Double> lowerLimit = new ArrayList<>(getNumberOfVariables()) ;
+        List<Double> upperLimit = new ArrayList<>(getNumberOfVariables()) ;
+        int y = getNumberOfVariables();
+        for (int i = 0; i < getNumberOfVariables(); i++) {
+            lowerLimit.add(-5.0);
+            upperLimit.add(5.0);
+        }
+
+        setLowerLimit(lowerLimit);
+        setUpperLimit(upperLimit);
+
+        Debug.OUT("AntiSpamFilterProblem [Constructor(INTEGER)]");
+    }
+
+    //ver metodo rules a entrar vazias.
+    /** Evaluate() method */
+    public void evaluate(DoubleSolution solution){
+        Debug.IN("AntiSpamFilterProblem [evaluate(DoubleSolution)]");
+
+        //tratamento de dados
+        int iterator = 0;
+        for (Entry<String, Double> rule : rules.entrySet()){
+            rule.setValue(solution.getVariableValue(iterator));
+            iterator++;
+        }
+
+
+        Debug.msg("Call evaluate(rules)");
+        double [] fx = evaluate(rules);
+
+        solution.setObjective(0, fx[0]); //objective 0 fx[0] will be subst by FN
+        solution.setObjective(1, fx[1]); //objective 1 fx[1] will be subst by FP
+        Debug.OUT("AntiSpamFilterProblem [evaluate(DoubleSolution)]");
+    }
+
+
+    public double[] evaluate(LinkedHashMap <String,Double> rules){
+       //CALL JAR
+        trow NotImplementedException;
+        double [] temp = {0.0};
+        return temp;
+    }
+
+
+    public LinkedHashMap<String, Double> getRules() {
+        return rules;
+    }
+
+
+    public LinkedHashMap<String, ArrayList<String>> getHam() {
+        return ham;
+    }
+
+
+    public LinkedHashMap<String, ArrayList<String>> getSpam() {
+        return spam;
+    }
+
+    //Manual calculation of FN and FP
+    public int[] getManualFN_FP(){
+        int[] result = new int[2];
+        return result;
+    }
+
+    public boolean validLists() {
+        return !rules.isEmpty() && !ham.isEmpty() && !spam.isEmpty();
+    }
+
+
+    public void setPathHam(String pathHam) {
+        this.pathHam = pathHam;
+    }
+
+
+    public void setPathSpam(String pathSpam) {
+        this.pathSpam = pathSpam;
+    }
+
+
+    public void setPathrules(String pathrules) {
+        this.pathrules = pathrules;
+    }
+
+}
