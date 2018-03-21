25b6034c61431115f64e8002462fe150404645f1
Gonçalo Matos <goncalo.matos@angry.ventures>
2018-03-21 11:39:30 +0000

    Added fixed header

--- src/main/resources/scripts/addMainComponents.js
+++ src/main/resources/scripts/addMainComponents.js
@@ -12,3 +12,20 @@ $('#topHeader').append($('<table cellspacing="30">\n' +
     '            <a class="linkHeader" href="supportView.html">SUPPORT</a>\n' +
     '        </th>\n' +
     '    </table>'));
+
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("topHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+
+$('#bottomFooter').append($());
