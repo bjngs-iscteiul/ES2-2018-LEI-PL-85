39dc38cfddab782a2cafe71d1cff4cedd4314d06
Gonçalo Matos <goncalo.matos@angry.ventures>
2018-03-21 11:50:42 +0000

    Added footer

--- src/main/resources/scripts/addMainComponents.js
+++ src/main/resources/scripts/addMainComponents.js
@@ -14,6 +14,32 @@ $('#topHeader').append($('<table cellspacing="30">\n' +
     '    </table>'));
 
 
+
+$('#bottomFooter').append($('<table cellspacing="30">\n' +
+    '        <tr>\n' +
+    '            <th><h2>About the tool</h2></th>\n' +
+    '            <th><h2>FAQ</h2></th>\n' +
+    '            <th><h2>Legal</h2></th>\n' +
+    '        </tr>\n' +
+    '        <tr>\n' +
+    '            <th><a class="footerLinks">Why use it?</a></th>\n' +
+    '            <th><a class="footerLinks">Most asked questions</a></th>\n' +
+    '            <th><a class="footerLinks">Privacy</a></th>\n' +
+    '        </tr>\n' +
+    '        <tr>\n' +
+    '            <th><a class="footerLinks">How it works?</a></th>\n' +
+    '            <th><a class="footerLinks">How to add a problem</a></th>\n' +
+    '            <th><a class="footerLinks">Terms of service</a></th>\n' +
+    '        </tr>\n' +
+    '        <tr>\n' +
+    '            <th><a class="footerLinks">Explore more about</a></th>\n' +
+    '            <th><a class="footerLinks">Can\'t upload JAR</a></th>\n' +
+    '            <th><a class="footerLinks">Usage Plan Terms</a></th>\n' +
+    '        </tr>\n' +
+    '\n' +
+    '    </table>'));
+
+
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("topHeader");
@@ -27,5 +53,3 @@ function myFunction() {
     }
 }
 
-
-$('#bottomFooter').append($());
